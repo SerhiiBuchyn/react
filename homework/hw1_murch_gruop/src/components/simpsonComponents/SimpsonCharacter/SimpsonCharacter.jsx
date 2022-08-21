c7336a8643cb3501a5beb0fd5d60eb6ad158f495
SerhiiBuchyn <serhii.buchyn@gmail.com>
2022-08-21 20:35:48 +0300

    rename some props in this files

--- homework/hw1_murch_gruop/src/components/simpsonComponents/SimpsonCharacter/SimpsonCharacter.jsx
+++ homework/hw1_murch_gruop/src/components/simpsonComponents/SimpsonCharacter/SimpsonCharacter.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './SimpsonCharacter.css';
 
-const SimpsonCharacter = ({name, description, photo}) => {
+const SimpsonCharacter = ({name, surname, photo}) => {
     return (
         <div className="simpson__character d-flex">
             <h2 className="title">{name}</h2>
-            <p className="description">{description}</p>
+            <p className="surname">{surname}</p>
             <div className="img__container">
                 <img className="simpson__img" src={photo} alt="logo"/>
             </div>
--- homework/hw1_murch_gruop/src/components/simpsonComponents/SimpsonComponent/SimpsonComponent.jsx
+++ homework/hw1_murch_gruop/src/components/simpsonComponents/SimpsonComponent/SimpsonComponent.jsx
@@ -6,23 +6,23 @@ const SimpsonComponent = () => {
         <div className="simpson__wrapper">
             <SimpsonCharacter
                 name={"Bart"}
-                description={"Simpson"}
+                surname={"Simpson"}
                 photo={"https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png"}/>
             <SimpsonCharacter
                 name={"Homer"}
-                description={"Simpson"}
+                surname={"Simpson"}
                 photo={"https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png"}/>
             <SimpsonCharacter
                 name={"Marge"}
-                description={"Simpson"}
+                surname={"Simpson"}
                 photo={"https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png"}/>
             <SimpsonCharacter
                 name={"Lisa"}
-                description={"Simpson"}
+                surname={"Simpson"}
                 photo={"https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png"}/>
             <SimpsonCharacter
                 name={"Maggie"}
-                description={"Simpson"}
+                surname={"Simpson"}
                 photo={"https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png"}/>
 
 
