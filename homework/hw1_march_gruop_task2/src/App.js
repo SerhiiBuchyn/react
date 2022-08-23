d184911006b108219fdbf6ab46291bac2b40dd3a
SerhiiBuchyn <serhii.buchyn@gmail.com>
2022-08-24 00:31:57 +0300

    add code

--- homework/hw1_march_gruop_task2/src/App.js
+++ homework/hw1_march_gruop_task2/src/App.js
@@ -1,11 +1,13 @@
+import Characters from '../src/components/Characters/Characters';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="wrapper">
-
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Characters/>
+        </div>
+    );
 }
 
 export default App;
